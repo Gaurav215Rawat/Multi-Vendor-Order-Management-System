ce0e763afed57e4881ba1cde1e0753e730819e4d
Gaurav Rawat <123564858+Gaurav215Rawat@users.noreply.github.com>
2025-05-04 23:38:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+// server.js
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const app = express();
+
+// Import routes
+const authRoutes = require('./routes/auth');
+const productRoutes = require('./routes/product');
+const orderRoutes = require('./routes/order');
+const analyticsRoutes = require('./routes/analytics');
+const userRoutes = require('./routes/user');
+const subOrderRoutes = require('./routes/suborder');
+
+
+// Middleware
+const { errorHandler } = require('./middleware/error');
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/analytics', analyticsRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/suborders', subOrderRoutes);
+
+// Error handling middleware
+app.use(errorHandler);
+
+// Connect to MongoDB and start server
+const PORT = process.env.PORT || 5000;
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log('✅ Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+})
+.catch((err) => {
+  console.error('❌ MongoDB connection failed:', err.message);
+  process.exit(1);
+});
+
